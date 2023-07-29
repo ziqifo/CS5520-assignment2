70e36996dbac410a1e11930fd4083a1851a7a847
ziqifo <fo.z@northeastern.edu>
2023-07-28 23:05:16 -0700

    Update App.js

--- assignment2/App.js
+++ assignment2/App.js
@@ -1,20 +1,116 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import EntryList from './component/EntryList';
+import AllEntry from './screen/AllEntriesScreen';
+import OverEntry from './screen/OverlimitEntriesScreen';
+import AddEntry from './screen/AddEntryScreen';
+import EditEntry from './screen/EditEntryScreen';
+import ButtonPressable from './component/ButtonPressable';
+import { useState } from 'react';
+import myStyling from './styles';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
+
+  const Tab = createBottomTabNavigator();
+  const Stack = createStackNavigator();
+  const [limit, setLimit] = useState(500);
+
+  const TabNavi = ( {navigation} ) => (
+    <Tab.Navigator>
+      <Tab.Screen 
+        name="All" 
+        component={AllEntry} 
+        initialParams={{ 
+          titleText: 'All Entries'
+        }}
+        options={({ route }) => {
+          return {
+            headerRight: () => {
+              return (
+                <ButtonPressable
+                  onPressed={() => {navigation.navigate('Add', { limit: limit });}}
+                >
+                  <AntDesign name="pluscircleo" size={16} color={myStyling.black} />
+                </ButtonPressable>
+              );
+            },
+            tabBarIcon: ({ focused, color, size }) => {
+              return (
+                <AntDesign
+                name={focused ? "infocirlce" : "infocirlceo"}
+                size={18} 
+                color={focused ? myStyling.red : myStyling.black}
+                />
+              );
+            },
+            tabBarLabel: () => {
+              return (
+                <Text>All Entries</Text>
+              );
+            },
+          };
+        }}
+      />
+      <Tab.Screen name="Over" component={OverEntry} initialParams={{ 
+          titleText: 'Over-Limit Entries'
+        }}
+        options={({ route }) => {
+          return {
+            headerRight: () => {
+              return (
+                <ButtonPressable
+                  onPressed={() => {navigation.navigate('Add', { limit: limit });}}
+                >
+                  <AntDesign name="pluscircleo" size={16} color={myStyling.black} />
+                </ButtonPressable>
+              );
+            },
+            tabBarIcon: ({ focused, color, size }) => {
+              return (
+                <AntDesign 
+                  name={focused ? "clockcircle" : "clockcircleo"}
+                  size={18} 
+                  color={focused ? myStyling.red : myStyling.black} />
+              );
+            },
+            tabBarLabel: () => {
+              return (
+                <Text>Over-Limit Entries</Text>
+              );
+            },
+          };
+        }}
+        />
+    </Tab.Navigator>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer
+        theme={DefaultTheme}
+      >
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={TabNavi}
+            options={({ route }) => {
+              return {
+                headerShown: false
+              };
+            }}
+          />
+          <Stack.Screen
+            name="Add"
+            component={AddEntry}
+          />
+          <Stack.Screen
+            name="Edit"
+            component={EditEntry}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
